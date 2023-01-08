644f97805378747dc00164d58f440f2c231943bf
ArAs101 <assadzarm@gmail.com>
2023-01-08 21:26:41 +0100

    Verlinkungen bearbeiten!!! Habe alle aktuellen Dateien erstellt, Spielfeld, Spielereigenschaften, ...

---
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainMenu.fxml")));
+            Scene scene = new Scene(root);
+            //stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/javafx/Singleplayer.java
@@ -0,0 +1,24 @@
+package com.example.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Singleplayer {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void GameSP(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainMenu.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
